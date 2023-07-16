7772f2d41789c074fe2e8e17a8d3096f6124fc0f
CH4019 <77958033+CH4019@users.noreply.github.com>
2023-07-16 17:59:08 +0800

    恢复

--- app/src/main/java/com/ch4019/jdaapp/model/RxHttpRepository.kt
+++ app/src/main/java/com/ch4019/jdaapp/model/RxHttpRepository.kt
@@ -1,20 +1,28 @@
 package com.ch4019.jdaapp.model
 
+import android.util.Log
 import kotlinx.coroutines.flow.first
 import rxhttp.toFlow
 import rxhttp.wrapper.coroutines.CallFlow
 import rxhttp.wrapper.param.RxHttp
 import java.text.DecimalFormat
 import javax.inject.Inject
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.retry
+
 
 // 从指定api获取json数据并转为kotlin,传递给viewModel的检查更新部分
 class RxHttpRepository @Inject constructor() {
-    suspend fun getNewVersionCode(): VersionCode{
+    suspend fun getNewVersionCode(): AppVersion {
         return RxHttp.get("https://jdaassistant.ch4019.fun/newApp.json")
-            .toFlow<VersionCode>()
+            .toFlow<AppVersion>()
+            .catch {
+                Log.e("RxHttpRepository", "getNewVersionCode: ${it.message}")
+            }
+            .retry(5)
             .first()
     }
-    
+
     fun getRxHttp(): CallFlow<QuoteList> {
         return RxHttp.get("https://api.github.com/repos/CH4019/xinximenhu/releases/latest")
             .toFlow()
